f22e370fb5622e1a04f1daed7b1b3a9d2cc62f34
Deusdete Ferreira <deuzinh2010@gmail.com>
2023-05-31 23:31:55 -0300

    Add file-saver

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["upcdn.io", "replicate.delivery", "lh3.googleusercontent.com"],
+    domains: ["localhost", "upcdn.io", "replicate.delivery", "lh3.googleusercontent.com"],
   },
   typescript: {
     // !! WARN !!
